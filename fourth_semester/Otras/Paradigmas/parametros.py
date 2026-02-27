61e841c09bd062d4d720ae221d2d18e52b7a5dae
Itiel Alvarez <alfred.delgadillo@ulv.edu.mx>
2026-02-27 10:27:35 -0600

    prueba

---
+++ fourth_semester/Otras/Paradigmas/parametros.py
@@ -0,0 +1,34 @@
+# Preguntas y respuestas ===============================================================================================
+
+'''
+1. ¿Las funciones internas necesitan argumentos?
+R= Algunas
+
+2. En una función ¿Como se le denomina a un argumento asignado a una variable?
+R= Parametro
+
+3. ¿Cuando se evalua el argumento en una función?
+R= Antes de que la función sea llamada
+
+4. ¿Cuantas veces se evaluan los argumentos?
+R= Una sola vez
+
+5. En la función se tiene una variable como argumento con el nombre POO, pero 
+   alejecutar le paso el argumento paradigma, esto afectara? 
+R= No, lo unico que podria dañarsera el type.
+
+6. ¿En que tipo de funciones puedo utilizar las reglas de composición?
+R= Internas y Externas
+
+7. ¿Las funciones pueden recibir unicamente un parametro?
+R= No, eso depende de lo que se necesecite en la función
+
+8. ¿Para que tipo de función se necesita utilizar el def?
+R= externas/definidas por el usuario
+
+9. ¿Se puede utilizar una función como argumento de otra?
+R= Si
+
+10. Si quiero utilizar una variable como argumento en mi función, ¿Que debo de hacer antes con ella? 
+R= Definirla
+'''
---
+++ fourth_semester/Relaciones.js
@@ -0,0 +1,5 @@
+//* Existen dos tipos:
+
+//! Embedding de uno a algunos <= 16 MB 
+
+//? Referenciales guardar el _id del documento relacionado
